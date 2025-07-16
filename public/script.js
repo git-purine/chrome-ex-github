9d2fcb88ede12fc2060ef466957bd06a588df4ee
git-purine <gmai1.hiro@gmail.com>
2025-07-16 11:22:51 +0900

    update addButton_CreatePullRequestToDev

--- public/script.js
+++ public/script.js
@@ -8,14 +8,18 @@ function addButton_CreatePullRequestToDev() {
   var headerMetas = document.querySelectorAll('div.gh-header-meta')
   if (headerMetas.length > 0) {
     var headerMeta = headerMetas[0]
+    var baseRef = headerMeta.querySelector('span.base-ref>a>span').innerText
+    var headRef = headerMeta.querySelector('span.head-ref>a>span').innerText
+    console.log('headRef', headRef, 'baseRef', baseRef)
+    if (baseRef === "main") {
     headerMeta.insertBefore(button, headerMeta.firstElementChild)
+    }
   }
 
   button.onclick = async () => {
     console.log('chrome-ex-github.script.js.onclick', headerMetas)
-    var headRef = headerMeta.querySelector('span.head-ref>a>span').innerText
     var locationHref = window.location.href
-    console.log('headRef', headRef, 'locationHref', locationHref)
+    console.log('locationHref', locationHref)
 
     var [url, prId] = locationHref.split("/pull/")
     url += "/compare/develop..." + headRef + `?quick_pull=1&body=%23${prId}`
