70900df089e76770e33b1a98c64318d5978c45e7
git-purine <gmai1.hiro@gmail.com>
2025-07-16 12:06:24 +0900

    add addButton_CreateGoogleCalendarSchedule

--- public/script.js
+++ public/script.js
@@ -12,7 +12,7 @@ function addButton_CreatePullRequestToDev() {
     var headRef = headerMeta.querySelector('span.head-ref>a>span').innerText
     console.log('headRef', headRef, 'baseRef', baseRef)
     if (baseRef === "main") {
-    headerMeta.insertBefore(button, headerMeta.firstElementChild)
+      headerMeta.insertBefore(button, headerMeta.firstElementChild)
     }
   }
 
@@ -27,7 +27,39 @@ function addButton_CreatePullRequestToDev() {
 
     open(url, '_blank')
   }
-
 }
-
 addButton_CreatePullRequestToDev();
+
+function addButton_CreateGoogleCalendarSchedule() {
+  var button = document.createElement('button')
+  button.className = 'flex-shrink-0 flex-self-start flex-md-self-center Button--secondary Button--small Button m-0 mr-md-2 mb-2'
+  button.innerText = "Add to GCal"
+
+  var headerTitles = document.querySelectorAll('h1.gh-header-title>bdi')
+  var headerMetas = document.querySelectorAll('div.gh-header-meta')
+  if (headerTitles.length > 0 && headerMetas.length > 0) {
+    var headerTitle = headerTitles[0]
+    var headerMeta = headerMetas[0]
+    var results = headerTitle.innerText.match(/\d{4}-?\d{2}-?\d{2}/g)
+    console.log('headerTitle', headerTitle, 'results', results)
+    if (results && results.length > 0) {
+      headerMeta.insertBefore(button, headerMeta.firstElementChild)
+    }
+  }
+
+  button.onclick = async () => {
+    console.log('chrome-ex-github.script.js.onclick', results)
+    var date = results[0]
+    var url = "https://calendar.google.com/calendar/event?"
+    url += "action=TEMPLATE"
+    url += "&text=Release " + encodeURIComponent(window.location.href.split("/")[4])
+    url += "&details=" + window.location.href
+    url += "&dates=" + date.replace(/-/g, '') + "T090000Z/" + date.replace(/-/g, '') + "T093000Z"
+    url += "&add="
+    url += "&src="
+    console.log('url', encodeURI(url))
+
+    open(url, '_blank')
+  }
+}
+addButton_CreateGoogleCalendarSchedule();
